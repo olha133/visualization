05345fcaabc36e01ac28e5ad64eaeef611ed8e88
olha133 <olha.shvenda@student.tuke.sk>
2024-05-23 23:05:33 +0200

    dynamic visualization with animation; control num_cities

--- playground/views.py
+++ playground/views.py
@@ -14,38 +14,38 @@ def extract_number(string):
     return int(match.group(1)) if match else -1
 
 def hc(request):
-    context = dict()
-        
-    # Clean up old plot files
-    plots_dir = os.path.join(settings.MEDIA_ROOT, 'plots')
-    # for file in os.listdir(plots_dir):
-    #     file_path = os.path.join(plots_dir, file)
-    #     if os.path.isfile(file_path):
-    #         os.unlink(file_path)
+    if request.method == 'POST':
+        num_cities = int(request.POST.get('city_count'))
+        context = dict()
             
-    # num_cities = random.randint(5, 20)
-    # hc = HillClimbing(num_cities)
-    # tour, distance = hc.run()
-    tour = [1,2,3,4]
-    distance = 0.5
+        # Clean up old plot files
+        plots_dir = os.path.join(settings.MEDIA_ROOT, 'plots')
+        for file in os.listdir(plots_dir):
+            file_path = os.path.join(plots_dir, file)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+                
+        hc = HillClimbing(num_cities)
+        tour, distance = hc.run()
 
-    # Gather plot file paths
-    plot_files = [
-        os.path.join(settings.MEDIA_URL, 'plots', file).replace('\\', '/')
-        for file in os.listdir(plots_dir)
-        if file.endswith('.png')
-    ]
-    plot_files = sorted(plot_files, key=extract_number)
-    # print(type(plot_files))
+        # Gather plot file paths
+        plot_files = [
+            os.path.join(settings.MEDIA_URL, 'plots', file).replace('\\', '/')
+            for file in os.listdir(plots_dir)
+            if file.endswith('.png')
+        ]
+        plot_files = sorted(plot_files, key=extract_number)
+        # print(type(plot_files))
 
-    context.update({
-        'tour': tour,
-        'distance': distance,
-        'plot_files': plot_files
-    })
-    # print(context['plot_files'])
-
-    return render(request, 'playground/hc.html', context)
+        context.update({
+            'tour': tour,
+            'distance': distance,
+            'plot_files': plot_files
+        })
+        # print(context['plot_files'])
+        return JsonResponse(context)
+    else:
+        return render(request, 'playground/hc.html')
 # def hc(request):
 #     return render(request, "playground/hc.html", {"algorithms": algorithms})
 
