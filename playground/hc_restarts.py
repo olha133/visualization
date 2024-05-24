908a235b6787fc3de9976efc1508738104f73a7d
olha133 <olha.shvenda@student.tuke.sk>
2024-05-24 22:14:26 +0200

    hc_restarts_static

---
+++ playground/hc_restarts.py
@@ -0,0 +1,124 @@
+import random
+import numpy as np
+import time
+import pandas as pd
+import os
+import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+from django.conf import settings
+
+class HillClimbingRestarts:
+    def __init__(self, num_cities=5, num_runs=1, csv_file=None):
+        self.num_cities = num_cities
+        self.num_runs = num_runs
+        self.csv_file = csv_file
+        self.positions, self.distance_matrix = self.generate_distance_matrix()
+        self.random_seed = int(time.time())
+        random.seed(self.random_seed)
+        self.plot_counter = 0
+
+    def generate_distance_matrix(self):
+        # If CSV file exists, read it. Otherwise, generate random positions and calculate the distance matrix
+        if self.csv_file and os.path.exists(self.csv_file):
+            data = pd.read_csv(self.csv_file, header=0).values
+            positions = data[:, :2]
+            distance_matrix = squareform(pdist(positions, 'euclidean'))
+            self.num_cities = len(distance_matrix)
+        else:
+            positions = np.random.rand(self.num_cities, 2)
+            distance_matrix = squareform(pdist(positions, 'euclidean'))
+        return positions, distance_matrix
+
+    def total_distance(self, tour):
+        return sum(self.distance_matrix[tour[i], tour[i + 1]] for i in range(len(tour) - 1)) + self.distance_matrix[tour[-1], tour[0]]
+
+    def get_neighbors(self, tour):
+        neighbors = []
+        for i in range(1, len(tour) - 2):  # Exclude the first and last city
+            new_tour = tour[:]
+            new_tour[i], new_tour[i + 1] = new_tour[i + 1], new_tour[i]  # Swap two adjacent cities
+            neighbors.append(new_tour)
+        return neighbors
+    
+    def plot_graph_step(self, G, positions, tour=None, edge_colors='#3E5CC5', node_colors='#65B48E',swapped_edges=None, swapped_nodes=None):
+        plt.clf()
+        pos = {i: (positions[i][0], positions[i][1]) for i in range(len(positions))}
+        
+        if tour:
+            path_edges = list(zip(tour, tour[1:]))
+        path_edges_background = list(G.edges)
+        nx.draw(G, pos, with_labels=False, edgelist=path_edges_background, edge_color='#dbdbdb', width=1.5)
+        labels = {i: i + 1 for i in G.nodes()}
+        nx.draw(G, pos, labels=labels, node_color=node_colors, node_size=350, font_size=10, edgelist=path_edges, edge_color=edge_colors, width=2)
+
+        if swapped_edges:
+            nx.draw_networkx_edges(G, pos, edgelist=swapped_edges, edge_color='#E64E00', width=2)
+        
+        if swapped_nodes:
+            nx.draw_networkx_nodes(G, pos, nodelist=swapped_nodes, node_color='#E6EB00', node_size=350)
+                # Save the plot
+        plot_dir = os.path.join(settings.MEDIA_ROOT, 'plots')
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+        plot_filename = os.path.join(plot_dir, f'plot_{self.plot_counter}.png')
+        plt.savefig(plot_filename, dpi=500)
+        
+        self.plot_counter += 1
+                
+    def generate_complete_graph(self):
+        G = nx.Graph()
+        n = len(self.distance_matrix)
+        for i in range(n):
+            for j in range(i + 1, n):  # Only add one direction for symmetric TSP
+                if self.distance_matrix[i][j] != 0:
+                    G.add_edge(i, j, weight=self.distance_matrix[i][j])
+        return G
+    
+    def run(self):
+        best_tour = None
+        best_distance = float('inf')
+        for _ in range(self.num_runs):
+            # Generate a random initial tour
+            tour = list(range(self.num_cities))
+            random.shuffle(tour)
+            tour.append(tour[0])
+            # Create a graph and visualize it
+            G = self.generate_complete_graph()
+            positions = {i: pos for i, pos in enumerate(self.positions)}
+                        
+            self.plot_graph_step(G, positions, tour)
+
+            while True:
+                neighbors = self.get_neighbors(tour)
+                current_distance = self.total_distance(tour)
+                neighbors_distances = [self.total_distance(neighbor) for neighbor in neighbors]
+
+                # If there's no improvement, break the loop
+                if min(neighbors_distances) >= current_distance:
+                    break
+                # Store the current tour before making a swap
+                old_tour = tour[:]
+
+                # Choose the neighbor with the smallest distance as the new tour
+                tour = neighbors[np.argmin(neighbors_distances)]
+                # Find the swapped cities
+                swapped_edges = []
+                swapped_nodes = []
+                for i in range(len(tour) - 1):
+                    if old_tour[i] != tour[i]:
+                        swapped_edges = [(tour[i-1], tour[i]), (tour[i+1], tour[i + 2])]
+                        swapped_nodes = [tour[i-1], tour[i], tour[i+1], tour[i+2]]
+                        break           
+                self.plot_graph_step(G, positions, tour, swapped_edges=swapped_edges, swapped_nodes=swapped_nodes)
+            
+            tour_distance = self.total_distance(tour)
+            if tour_distance < best_distance:
+                best_tour = tour
+                best_distance = tour_distance
+            
+            self.plot_graph_step(G, positions, tour)
+        self.plot_graph_step(G, positions, best_tour, edge_colors='#E64E00', node_colors='#f05100')
+        return best_tour, best_distance
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .hc import HillClimbing
+from .hc_restarts import HillClimbingRestarts
 import os
 import random
 from django.conf import settings
@@ -73,7 +74,52 @@ def hc(request):
 
 
 def hc_restarts(request):
-    return render(request, "playground/hc_restarts.html")
+    if request.method == 'POST':
+        if 'custom_file' in request.FILES:
+            uploaded_file = request.FILES['custom_file']
+            filepath = os.path.join(settings.BASE_DIR, 'data', 'custom_maps', uploaded_file.name)
+            with open(filepath, 'wb+') as destination:
+                for chunk in uploaded_file.chunks():
+                    destination.write(chunk)
+            is_valid, error_message = validate_csv(filepath)
+            if not is_valid:
+                return JsonResponse({'error': error_message}, status=400)
+        else:
+            num_cities = int(request.POST.get('city_count'))
+            filename = f'tour{num_cities}.csv'
+            filepath = os.path.join(settings.BASE_DIR, 'data', 'defined_maps', filename)
+        # print(filepath)
+        context = dict()
+        iterations = int(request.POST.get('iterations', 1)) # Get the number of iterations
+
+        # Clean up old plot files
+        plots_dir = os.path.join(settings.MEDIA_ROOT, 'plots')
+        for file in os.listdir(plots_dir):
+            file_path = os.path.join(plots_dir, file)
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+                
+        hc = HillClimbingRestarts(num_runs = iterations, csv_file = filepath)
+        tour, distance = hc.run()
+
+        # Gather plot file paths
+        plot_files = [
+            os.path.join(settings.MEDIA_URL, 'plots', file).replace('\\', '/')
+            for file in os.listdir(plots_dir)
+            if file.endswith('.png')
+        ]
+        plot_files = sorted(plot_files, key=extract_number)
+        # print(type(plot_files))
+
+        context.update({
+            'tour': tour,
+            'distance': distance,
+            'plot_files': plot_files
+        })
+        # print(context['plot_files'])
+        return JsonResponse(context)
+    else:
+        return render(request, 'playground/hc_restarts.html')
 
 def hc_larger_radii(request):
     return render(request, "playground/hc_larger_radii.html")
